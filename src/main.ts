ef50a8e1d40077fe6ec3c1ff46c2e9d882e14062
NemesisUA <70847593+NemesisUA@users.noreply.github.com>
2024-09-26 18:36:12 +0300

    Update main.ts

--- src/main.ts
+++ src/main.ts
@@ -21,7 +21,7 @@ async function bootstrap() {
   app.enableCors({
     origin: '*',
     methods: 'GET,POST,OPTIONS,PUT,DELETE',
-    allowedHeaders: 'Origin,X-Requested-With,Content-Type,Accept,Authorization',
+    allowedHeaders: '*',
   });
   
   SwaggerModule.setup('api', app, document);
